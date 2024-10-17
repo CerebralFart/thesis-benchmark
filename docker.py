e457f7626d8a74fe0a995e252513999585e81fbb
Sven Mol <sven@taupeweb.nl>
2024-10-17 11:20:58 +0200

    Docker helper

---
+++ docker.py
@@ -0,0 +1,27 @@
+from subprocess import DEVNULL, PIPE, Popen
+
+
+class Container:
+    def __init__(self, opts):
+        self._process = Popen(['docker', 'run', '-d'] + opts.split(" "),
+                              stderr=PIPE,
+                              stdout=PIPE,
+                              text=True,
+                              )
+        self._process.wait()
+        self._docker_id = self._process.stdout.read().strip()
+
+    def log(self):
+        process = Popen(['docker', 'logs', self._docker_id], stdout=PIPE, text=True)
+        process.wait()
+        return process.stdout.read()
+
+    def remove(self):
+        process = Popen(['docker', 'rm', self._docker_id], stdout=DEVNULL)
+        process.wait()
+
+
+if __name__ == '__main__':
+    container = Container('hello-world')
+    print(container.log())
+    container.remove()
