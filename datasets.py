ad3fac6e890a0200be9e5e6f3ab0148d06eca656
Sven Mol <sven@taupeweb.nl>
2024-11-28 12:48:21 +0100

    Remove bsbm 10b as it is too large

--- datasets.py
+++ datasets.py
@@ -3,5 +3,4 @@ datasets = {
     'BSBM 10M': './datasets/bsbm-10m.ttl',
     'BSBM 100M': './datasets/bsbm-100m.ttl',
     'BSBM 1B': './datasets/bsbm-1b.ttl',
-    'BSBM 10B': './datasets/bsbm-10b.ttl',
 }
